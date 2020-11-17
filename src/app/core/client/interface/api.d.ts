4db1c754d1b1be89bb95ba0363c75d71dae13177
pinecut <www.353376352@qq.com>
2020-11-18 01:00:32 +0800

    canvas绘制基础逻辑

--- src/app/core/client/interface/api.d.ts
+++ src/app/core/client/interface/api.d.ts
@@ -1,4 +1,13 @@
-
 declare interface AddProject {
+  id: string;
   name: string;
+  intro: string;
+}
+
+declare interface AddPerson {
+  id: string;
+  name: string;
+  gender: number;
+  birth: number;
+  intro: string;
 }
