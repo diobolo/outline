a6e9386f222de713fb660cd1d52e483cfcf7f840
diobolo <www.353376352@qq.com>
2020-12-02 11:50:09 +0800

    左右拖拽效果

--- src/app/project/event/timeline/timeline.component.ts
+++ src/app/project/event/timeline/timeline.component.ts
@@ -1,8 +1,8 @@
-import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {ClientService} from '../../../core/client/service/client.service';
-import {Subject} from 'rxjs';
-import {debounce, debounceTime, throttleTime} from 'rxjs/operators';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ClientService } from '../../../core/client/service/client.service';
+import { Subject } from 'rxjs';
+import { debounce, debounceTime, throttleTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-timeline',
@@ -10,7 +10,7 @@ import {debounce, debounceTime, throttleTime} from 'rxjs/operators';
   styleUrls: ['./timeline.component.scss']
 })
 export class TimelineComponent implements OnInit, OnDestroy {
-  @ViewChild('canvas', {static: true}) canvas: ElementRef;
+  @ViewChild('canvas', { static: true }) canvas: ElementRef;
   ctx: CanvasRenderingContext2D;
   eventList: Event[] = [];
   subject: Subject<any> = new Subject<any>();
@@ -19,6 +19,11 @@ export class TimelineComponent implements OnInit, OnDestroy {
     x: 0,
     y: 0
   };
+  offset = {
+    x: 0,
+    y: 0
+  };
+  scale = 100;  // 表示每单位显示多少小时的内容, 每单位长度暂定100px
 
   constructor(private route: ActivatedRoute,
               private client: ClientService) {
@@ -26,13 +31,14 @@ export class TimelineComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    this.getEventList(this.pid);
+    this.getEventList(this.pid).then(() => {
+      this.resetView();
+    });
     window.addEventListener('resize', this.onResize.bind(this));
     this.subject.pipe(throttleTime(100)).subscribe(() => {
       this.resetView();
     });
     this.ctx = this.canvas.nativeElement.getContext('2d');
-    this.resetView();
   }
 
   ngOnDestroy(): void {
@@ -42,22 +48,25 @@ export class TimelineComponent implements OnInit, OnDestroy {
   }
 
   draw(): void {
+    const width = this.canvas.nativeElement.width;
+    const height = this.canvas.nativeElement.height;
     this.ctx.fillStyle = 'lightseagreen';
-    this.ctx.fillRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+    this.ctx.fillRect(0, 0, width, height);
     this.ctx.strokeStyle = 'white';
     this.ctx.lineWidth = 2;
     this.ctx.beginPath();
-    this.ctx.moveTo(0, this.canvas.nativeElement.height / 2);
-    this.ctx.lineTo(this.canvas.nativeElement.width, this.canvas.nativeElement.height / 2);
+    this.ctx.moveTo(0, height / 2);
+    this.ctx.lineTo(width, height / 2);
     this.ctx.closePath();
+    this.ctx.arc(width / 2 + this.offset.x, height / 2, 5, 0, Math.PI * 2, false);
     this.ctx.stroke();
   }
 
-  getEventList(pid): void {
+  getEventList(pid): Promise<any> {
     if (!pid) {
       return;
     }
-    this.client.getEventList(pid).then(res => {
+    return this.client.getEventList(pid).then(res => {
       this.eventList = res;
     });
   }
@@ -72,4 +81,26 @@ export class TimelineComponent implements OnInit, OnDestroy {
     this.draw();
   }
 
+  onMousedown(e: any): void {
+    e.target.offsetX = e.offsetX - this.offset.x;
+  }
+
+  onMousemove(e): void {
+    if (e.target.offsetX) {
+      this.offset.x = e.offsetX - e.target.offsetX;
+      this.draw();
+    }
+  }
+
+  onMouseup(e: any): void {
+    e.target.offsetX = 0;
+  }
+
+  onMousewheel(e): void {
+    if (e.wheelDelta < 0) {
+      this.scale++;
+    } else {
+      this.scale--;
+    }
+  }
 }
